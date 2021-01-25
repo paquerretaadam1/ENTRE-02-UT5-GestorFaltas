56c45cd0794bebbb85d1e3fdf6af3edff104ad69
Pedro Aquerreta <paquerretaadam1@gmail.com>
2021-01-25 19:27:37 +0100

    Clase estudiante

--- Estudiante.java
+++ Estudiante.java
@@ -8,7 +8,7 @@ public class Estudiante {
     private String apellidos;
     private int faltasNoJustificadas;
     private int faltasJustificadas;
-
+    private int apercibimiento;
     /**
      *  
      *  Inicializa los atributos a partir de la información recibida
@@ -17,10 +17,48 @@ public class Estudiante {
      *  
      */
     public Estudiante(String lineaDatos) {
-         
-
+        String[] datos = lineaDatos.split(SEPARADOR); 
+        this.nombre = hacerNombre(datos[0]);
+        this.apellidos = hacerApellidos(datos[1]);
+        this.faltasNoJustificadas = Integer.parseInt(datos[2].trim());
+        this.faltasJustificadas = Integer.parseInt(datos[3].trim());
     }
 
+    /**
+     * 
+     */
+    private String hacerNombre(String nombre) {
+        String[] aFormatear = nombre.split(" ");
+        int numeroNombres = aFormatear.length;
+        StringBuilder resul = new StringBuilder("");
+        if(numeroNombres > 1){
+            for(int i = 0; i < numeroNombres - 1; i++){
+                if(!aFormatear[i].isEmpty()){
+                    resul.append(aFormatear[i].toUpperCase().charAt(0));
+                    resul.append(". ");
+                }
+            }
+        }
+        char capitalizada = aFormatear[numeroNombres - 1].toUpperCase().charAt(0);
+        resul.append(capitalizada);
+        resul.append(aFormatear[numeroNombres - 1].substring(1));
+        return resul.toString();
+    }
+
+    /**
+     * 
+     */
+    private String hacerApellidos(String apellidos) {
+        apellidos = apellidos.trim();
+        StringBuilder resul = new StringBuilder("");
+        String[] sinEspacios = apellidos.split(" ");
+        for(int i = 0; i < sinEspacios.length; i++){
+            if(!sinEspacios[i].isEmpty()){
+                resul.append(sinEspacios[i].toUpperCase() + " ") ;   
+            }
+        }
+        return resul.toString();
+    }
 
     /**
      * accesor para el nombre completo
@@ -93,19 +131,51 @@ public class Estudiante {
      * (ver enunciado)
      */
     public String toString() {
-        
-        return null;
+        StringBuilder str = new StringBuilder("");
+        String formateado = "";
+        for(int i = 0; i < 8; i++){
+            switch(i){
+                case 0:
+                formateado = "Apellidos y Nombre:";
+                break;
+                case 1:
+                formateado = this.apellidos + SEPARADOR + " " + this.nombre ;
+                break;
+                case 2:
+                formateado = "Faltas No Justificadas:";
+                break;
+                case 3:
+                formateado = "" + faltasNoJustificadas + "";
+                break;
+                case 4:
+                formateado = "Faltas Justificadas:";
+                break;
+                case 5:
+                formateado = "" + faltasJustificadas + "";
+                break;
+                case 6:
+                formateado = "Apercibimientos:";
+                break;
+                case 7:
+                formateado = "";
+                break;
+            }
 
+            str.append(String.format("%-25s", formateado));
+            if(i % 2 != 0){
+                str.append("\n");
+            }
+        }
+        return str.toString();
     }
 
-
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         Estudiante e1 = new Estudiante("  ander ibai  ,  Ruiz Sena , 12, 23");
         System.out.println(e1);
         System.out.println();
         Estudiante e2 = new Estudiante(
-                " pedro josé   andrés  ,  Troya Baztarrica , 42, 6 ");
-        System.out.println(e2);
+        " pedro josé   andrés  ,  Troya Baztarrica , 42, 6 ");
+        // System.out.println(e2);
         System.out.println();
         Estudiante e3 = new Estudiante("  Javier  ,  Suescun  Andreu , 39, 9 ");
         System.out.println(e3);
@@ -113,7 +183,6 @@ public class Estudiante {
         Estudiante e4 = new Estudiante("julen, Duque Puyal, 5, 55");
         System.out.println(e4);
         System.out.println();
-        
 
         System.out.println("---------------------------------");
         e1.justificar(3);
---
+++ TipoApercibimiento.java
@@ -0,0 +1,24 @@
+
+/**
+ * Enumeration class TipoApercibimiento - write a description of the enum class here
+ * 
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum TipoApercibimiento{
+    DIEZ(10), 
+
+    VEINTE(20), 
+
+    TREINTA(30);
+    private int numero;
+    
+    private TipoApercibimiento(int numero){
+        this.numero = numero;
+    }
+
+    public int getMaximo(){
+        return numero;
+    }
+    
+}
